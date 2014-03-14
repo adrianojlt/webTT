aee9c7a2d8f1f38a61426c1e56837eb467d21e68
adriano <adrianojlt@gmail.com>
2014-03-14 18:07:45 +0000

    ...

--- spms/tabnav.js
+++ spms/tabnav.js
@@ -61,15 +61,94 @@ if ( typeof Object.create !== 'function' ) {
 
 
 
+/**
+ * dialog navigation plugin
+ */
+;(function( $ , window , document , undefined ) { // keep all your code in a Clousure ...
+
+	var pluginName = 'spmsDialog';
+
+	$.fn[pluginName] = function(settings) {
+
+		settings = $.extend({}, $.fn[pluginName].defaultSettings, settings);
+
+		// keep jQuery method chaining ...
+		return this.each(function() {
+
+			var plugin = Object.create( Plugin );
+
+			plugin.init(settings, this);
+		}); 
+	}
+
+	$.fn[pluginName].defaultSettings = {
+		opt1		: 'val1',
+		type		: 'warning',
+		opt3		: 'val3'
+	};
+
+	var Plugin = {
+
+		init: function(settings, elem) {
+
+			global = this;
+
+			// reference to the plugin object
+			this.self = this;
+
+			// selected element
+			this.elem = elem;
+
+			// jQuery obj of the selected element 
+			this.$elem = $(this.elem);
+
+			this.settings = settings;
+
+			//if ( settings.type === 'warning' ) { }
+
+			this.insertDIVs();
+			this.tmp();
+		}, 
+
+		insertDIVs: function() {
+			this.$elem.append('<div class="dialog-fixed-container"> <div class="dialog-header"> Aviso </div> <div class="dialog-content"> <div class="dialog-image"> </div> <div class="dialog-message" style="font-weight: bold;"> Content </div> <div class="dialog-message"> Content </div> </div> <div class="dialog-footer"> </div> </div> </div>');
+		},
+
+		warning: function() {},
+		confirm: function() {},
+		error: function() {},
+
+
+		tmp: function() {
+
+		}
+	}
+})( jQuery , window , document );
+
+
+(function($) {
+	"use strict";
+})(jQuery);
+
+
+function confirmDialog() {
+
+} 
+
+function confirmWarning() {
+
+}
+
+function confirmError() {
+
+}
 
 
 window.onload = function() {
-
 	//var container = document.getElementById('tabcontainer');
 	//var tabs = container.querySelectorAll('.tabnav li');
 	//var tabs = document.getElementById('tabcontainer').querySelectorAll('.tabnav li');
 	//for ( var i = 0 ; i < tabs.length ; i++ ) { tabs[i].onclick = tabClicked; }
-
 	//jsEvent();
 }
 
