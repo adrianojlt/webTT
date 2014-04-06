afc208ecd729ca285232428b5e8fa1d2948e7182
unknown <adrianojlt@gmail.com>
2014-04-07 00:41:36 +0100

    ...

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,7 @@
 
 function closures() {
 
-	var fn = (function (x) { return function(y) { return x+y; } })(2) // returns a function ...
+	fn = (function (x) { return function(y) { return x+y; } })(2) // returns a function ...
 
 	var result  = fn(10); // 12
 
@@ -21,5 +21,7 @@ function hoisting() {
 	}
 
 	f();
+
+	return true;
 }
 
