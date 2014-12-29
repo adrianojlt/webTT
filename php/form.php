ccd6f7faa324728e9a938b1fd438ed01bd7c7757
adriano <adrianojlt@gmail.com>
2014-12-29 00:31:17 +0000

    get form info with php

---
+++ php/form.php
@@ -0,0 +1,21 @@
+<?php 
+
+require_once('libs/FirePHP.class.php');
+ob_start();
+$firephp = FirePHP::getInstance(true);
+
+class Test {}
+
+$obj = new Test();
+
+//var_dump($obj);
+$firephp->log($_POST, 'post');
+
+
+if ( isset($_POST['submit']) ) {
+	echo $_POST["mail"];	
+	echo $_POST["pass"];	
+}
+
+?>
+
