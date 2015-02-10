f51309005c3f51dd73510c56983fbf8f53204005
adriano <adrianojlt@gmail.com>
2015-02-10 00:43:02 +0000

    action to functions

--- pages/gymlogger/create.js
+++ pages/gymlogger/create.js
@@ -11,8 +11,22 @@ function MainController( $scope , $route ) {
 
 	var self = this;
 
-	$scope.exercices = [];
+	$scope.exercices = [
+		{ 
+			repetitions: [
+				{ 
+					weight:"",
+					rep:""
+				}
+			] 
+		}
+	];
+
+	//$scope.exercices = [];
+	//$scope.exercices
+	//$scope.exercices[0] = { repetitions: [] };
 	//$scope.exercices.repetitions = [];
+	//$scope.exercices.repetitions[0] = {"name":""};
 	$scope.repetitions = [];
 
 
@@ -20,13 +34,31 @@ function MainController( $scope , $route ) {
 	$scope.repetitions.push({weight: null, rep: null});
 	$scope.exercices.push({});
 
+	$scope.addExerciceInput = function() {
+		$scope.exercices.push({});
+	};
+
+	$scope.removeExerciceInput = function(indice) {
+		$scope.exercices.splice(indice,1);
+	};
+
+	$scope.addRepetitionInput = function(a) {
+		console.log(a);
+		$scope.repetitions.push({});
+	};
+
+	$scope.removeRepetitionInput = function($index) {
+		console.log($index);
+		if ( $scope.repetitions.length > 1 )
+			$scope.repetitions.splice($index,1);
+	};
+
 	$scope.save = function() {
-		//console.log('save');
-		//console.log($scope.exercices);
 		console.log($scope.repetitions);
 	};
 
 	$scope.cancel = function() {
 		console.log('cancel');
+		console.log($scope.exercices);
 	};
 }
