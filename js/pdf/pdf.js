7991196e85b5a43a31c15a7b86ea833f16bc76e0
adriano <adrianojlt@gmail.com>
2015-07-21 00:19:44 +0100

    generate pdf with jspdf javascript library

---
+++ js/pdf/pdf.js
@@ -0,0 +1,55 @@
+// ... dom is ready 
+$(document).ready(function() {
+
+	$('#print').click(function(evt) {
+		//console.log('print');
+		createPDF();
+	});
+
+	$('#fromHTML').click(function(evt) {
+		//console.log('print');
+		fromHTML();
+	});
+
+	function fromHTML() {
+    	var pdf = new jsPDF('p', 'in', 'letter');
+        var source = $('#testcase').first();
+        var specialElementHandlers = {
+            '#bypassme': function(element, renderer) {
+                return true;
+            }
+        };
+
+        pdf.fromHTML(
+        $('#container').get(0), // [Refer Exact code tutorial][2]HTML string or DOM elem ref.
+         	0.5,    // x coord
+         	0.5,    // y coord
+         	{
+             	'width': 7.5, // max width of content on PDF
+             	'elementHandlers': specialElementHandlers
+         });
+
+        pdf.output('dataurl');
+	};
+
+	function createPDF() {
+
+		var pdf = new jsPDF('p','pt','a4');
+		//var container = $('#container');
+		var container = document.getElementById('container');
+		var body = document.body;
+
+		var options = {pagesplit: true };
+
+		pdf.addHTML(body,0,0,options,function() {
+			//pdf.save('web.pdf');
+			//pdf.output('dataurl');
+			pdf.output('dataurlnewwindow');
+		});
+
+		//pdf.addHTML(document.body,function() {
+			//pdf.save('web.pdf');
+		//});
+	};
+
+});
