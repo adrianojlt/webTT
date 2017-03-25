ea8a37a0940ea8ba917e8df926f3815bd6201d04
Adriano Teixeira <adrianojlt@gmail.com>
2017-03-25 17:10:39 +0000

    removed phis

--- php/phis/config/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DB_SERVER', 	'localhost');
-define('DB_DATABASE', 	'phis');
-define('DB_USERNAME', 	'root');
-define('DB_PASSWORD', 	'');
-
-?>
--- php/phis/view/backoffice/logs.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-?>
-
-<div class="container">
-  <div class="row">
-	<h2>show logs here</h2>
-  </div>
-</div>
