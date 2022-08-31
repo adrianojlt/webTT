689f3ea3837401ef18c9f0be08f4111007543634
Adriano Leite <adriano.leite@farfetch.com>
2022-08-31 23:22:49 +0100

    feed datatables from json

--- pages/lynx/records.js
+++ pages/lynx/records.js
@@ -1,4 +1,44 @@
 
+var dataSet = [
+    [
+        "Google",
+        "10/08/2022 13:20",
+        "/folder/redirect-to-google",
+        "http://www.google.com",
+        "0.0.0.0",
+    ],
+    [
+        "Facebook",
+        "02/04/2022 03:20",
+        "/folder/redirect-to-facebook",
+        "http://www.facebook.com",
+        "0.0.0.0",
+    ],
+    [
+        "Google",
+        "02/08/2022 15:00",
+        "/folder/redirect-to-google",
+        "http://www.google.com",
+        "0.0.0.0",
+    ]
+];
+
 $(document).ready(function () {
-    $('#records').DataTable();
+    $('#records-js').DataTable({
+        //ajax: 'records.json'
+        data: dataSet,
+        columns: [
+            { title: 'Name' },
+            { title: 'Date' },
+            { title: 'Link',  },
+            { title: 'Redirect', render: function(data) { return '<a href="' + data + '">' + data + '</a>'; } },
+            { title: 'Ip' },
+        ],
+    });
+});
+
+$(document).ready(function () {
+    $('#recordsss').DataTable({
+        //ajax: 'records.json'
+    });
 });
